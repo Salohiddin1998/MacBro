8e0dee95710b0747c0223960d002a77b129923ed
Salohiddin1998 <abdulqosimovsalohiddin@gmail.com>
2022-10-13 01:43:38 +0500

    birinchi proyekt ochib oldim

---
+++ src/main/java/uz/pdp/macbro/MacBroApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.macbro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MacBroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MacBroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/pdp/macbro/MacBroApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.macbro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MacBroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
